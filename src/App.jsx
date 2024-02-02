4dab072ca6da65455ba062dbebe403eb8e12f959
dariuszewski <dariuszewski32@gmail.com>
2024-02-02 21:58:19 +0100

    accordion-based

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import './App.css'
-import Navbar from './components/navbar/Navbar';
-import Home from './components/home/Home';
-import About from './components/about/About';
-import Projects from './components/projects/Projects';
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Projects from "./components/projects/Projects";
+import Skills from "./components/skills/Skills";
 
 export default function App() {
   return (
     <div>
-        <Navbar />
-        <Home />
-        <About />
-        <Projects />
+      <Navbar />
+      <Home />
+      <About />
+      <Projects />
+      <Skills />
     </div>
   );
 }
