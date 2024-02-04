b0c84fd4283431b6fedbc23e1c210ddc9e203a8e
dariuszewski <dariuszewski32@gmail.com>
2024-02-04 19:00:49 +0100

    home-left

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,35 +1,33 @@
-import './home.css'
-import Typewriter from 'typewriter-effect';
+import "./home.css";
+import Typewriter from "typewriter-effect";
 
 export default function Home() {
-    return (
-        <div className='home' id="Home">
-
-            <div className="home_bottom">
-                <div className="static_text left">
-                    <div className="home_top">
-                        <div>Hi, I'm Dariu Szewski </div>               
-                    </div>
-                    <div>I'm a</div>         
-                    <Typewriter
-                    className='dynamic_text'
-                    options={{
-                        strings: [
-                            'Software Engineer',
-                            'Data Engineer',
-                            'Backend Developer', 
-                            'Data Analyst',
-                            'Web Developer',
-                        ],
-                        autoStart: true,
-                        loop: true,
-                    }}
-                    />  
-                </div>
-                <div className="right">
-                    <img className="home_img" src="src/assets/home4.png" alt="fail" />
-                </div>   
-            </div>        
+  return (
+    <div id="Home">
+      <div className="Home_container">
+        <div className="Home_text left">
+          <div className="Home_text_top">
+            <div>Hi, I'm Dariu Szewski </div>
+          </div>
+          <div>I'm a</div>
+          <Typewriter
+            options={{
+              strings: [
+                "Software Engineer",
+                "Data Engineer",
+                "Backend Developer",
+                "Data Analyst",
+                "Web Developer",
+              ],
+              autoStart: true,
+              loop: true,
+            }}
+          />
         </div>
-    )
+        <div className="right">
+          <img className="home_img" src="src/assets/home4.png" alt="fail" />
+        </div>
+      </div>
+    </div>
+  );
 }
